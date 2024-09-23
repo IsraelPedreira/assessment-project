52e21590b55d07925214a79b1d90045ecadc1e7e
Israel <israelped10@gmail.com>
2024-09-23 11:16:19 -0300

    fix: add validation to Login and Register DTOs

--- src/auth/dtos/register.dto.ts
+++ src/auth/dtos/register.dto.ts
@@ -1,6 +1,15 @@
+import { IsEmail, IsNotEmpty, IsStrongPassword, MinLength } from "class-validator";
+
 export class RegisterDto {
+    @IsNotEmpty()
+    @MinLength(3)
     username: string;
+
+    @IsStrongPassword()
     password: string;
+
+    @IsEmail()
     email: string;
+
     role: string;
 }
